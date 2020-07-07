5338e5ed674262df5faf2a48e4becdea173d3a54
InKronos <i15.piorkowski@gmail.com>
2020-07-07 23:28:51 +0200

    clearing the code

--- main.py
+++ main.py
@@ -2,64 +2,29 @@ from cv2 import cv2 as cv
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 import numpy as np
 import argparse
-from zipfile import ZipFile 
 import os
-import smtplib
-from sender2 import send_file_zipped
-
-def get_all_file_paths(directory): 
-  
-    # initializing empty file paths list 
-    file_paths = [] 
-  
-    # crawling through directory and subdirectories 
-    for root, directories, files in os.walk(directory): 
-        for filename in files: 
-            # join the two strings in order to form the full filepath. 
-            filepath = os.path.join(root, filename) 
-            file_paths.append(filepath) 
-  
-    # returning all file paths 
-    return file_paths         
 
 def printRegistration(name):
-    input = cv.imread("reg/rejestracja.png") 
+    #reading image in openCv
+    input = cv.imread("plates/LicensePlatePL.png") 
+    #converting it to PIL Image
     imageInPIL = Image.fromarray(np.uint8(input)).convert('RGBA')
     draw = ImageDraw.Draw(imageInPIL)
-    font = ImageFont.truetype('arklatrs.ttf', 88)
-    wordArray = name.split(" ")
-    #print(len(wordArray[0]))
-    i = 0
-    r = wordArray[0]
-    g = wordArray[1]
-    b = wordArray[2]
-    #print(r)
-    (x, y) = (50, 5)
-    for word in wordArray:
-        if len(wordArray[3]) == 3 and i == 4 and i > 2:
-            x = 230
-        elif i == 4 and  len(wordArray[3]) != 3 and i > 2 and len(wordArray[4]) > 3:
-            x = 210
-        elif i == 4 and  len(wordArray[5]) == 1 and i > 2:
-            x = 200
-        if i == 5 and i > 2:
-            x = 390
-        if i > 2:
-            message = word
-            color = f'rgb({r}, {g}, {b})'
-            print(color)
-            draw.text((x, y), message, fill=color, font = font)
-        i += 1
+    font = ImageFont.truetype('font/arklatrs.ttf', 88) #font and size of it
 
-    
+    (x, y) = (50, 5) #where string will be generated
+    message = name
+    color = 'rgb(0, 0, 0)' #remember when you convert to CV image program will think that you are using BGR
+    draw.text((x, y), message, fill=color, font = font)
+
+    #converting PIL Image to np array
     imageInCV = np.asarray(imageInPIL)
 
-    a = 0.13
+    a = 0.13    #size of small image
     r = (int(imageInCV.shape[1]*a), int(imageInCV.shape[0]*a))
-
     imag2 = cv.resize(imageInCV, r)
     height, width = imag2.shape[:2]
-    w, h = (20, 20)
+    w, h = (40, 40) #quality of new image (the bigger the better)
     #resize image
     imageInCV = cv.resize(imageInCV, r)
     #pixelizaiz of picture
@@ -71,24 +36,18 @@ def printRegistration(name):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', action='store', dest='simple_value',
-                        help='Store file value', default='dane2.txt')
-    parser.add_argument('-r', action='store', dest='recipient',
-                        help='Store recipient value', default='fnaticisthebest@gmail.com')
-    parser.add_argument('-s', action='store', dest='sender',
-                        help='Store sender value', default='i15.piorkowski@gmail.com')
+    parser.add_argument('-f', action='store', dest='fileValue',
+                        help='Store file value', default='data.txt')
 
 
     results = parser.parse_args()
-    file = open(results.simple_value, "r")
-    print(results.simple_value)
+    file = open(results.fileValue, "r")
     i = 1
     lines = file.readlines()
     for line in lines:
-        cv.imwrite(f'saved/{i}.png', printRegistration(line))
+        cv.imwrite(f'results/{i}.png', printRegistration(line)) #save image
         i += 1
     
-    send_file_zipped('saved', 'fnaticisthebest@gmail.com', 'i15.piorkowski@gmail.com')
 
 
 
